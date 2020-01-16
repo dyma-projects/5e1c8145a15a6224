2d2cf3aaf45f72e6e364e76a92c9d47aa43f704b
FM-O <florian.michel69250@gmail.com>
2020-01-16 17:30:58 +0100

    feat: released exercice2

--- src/app/exercice2/exercice2.component.ts
+++ src/app/exercice2/exercice2.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-exercice2',
@@ -7,10 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Exercice2Component implements OnInit {
   public valeur: string;
+  @ViewChild('val', {static:true}) inputVal:ElementRef;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  setValue():void {
+    this.valeur = this.inputVal.nativeElement.value;
   }
 
 }
